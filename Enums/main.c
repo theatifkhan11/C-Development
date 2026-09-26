ab2ea1a00a95a94a52a8f08bf10e71874adbfb05
Atif A. Khan <muhammadatifkhan.99@gmail.com>
2026-09-26 10:32:26 +0500

    passing command line arguments with the program using the main function

--- Enums/main.c
+++ Enums/main.c
@@ -1,37 +1,50 @@
 #include <stdio.h>
 
-int main() {
-    enum companies {GOOGLE,FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT};
+int main(int argc, char *argv[]) {
+    int numberOfArgument = argc;
+    char *argument1 = argv[0];
+    char *argument2 = argv[1];
 
-    //defining enums of type companies below
-
-    enum companies google = GOOGLE;
-    enum companies xerox  = XEROX;
-    enum companies ebay   = EBAY;
-
-    printf("The Company XEROX is: %d \n", xerox);
-
-    printf("The Company Google is: %d \n", GOOGLE);
-
-    printf("The Company EBAY is: %d \n", EBAY);
+    printf("The number of arguemnts to the program are: %d\n",numberOfArgument);
+    printf("The argument1 is the name of the program: %s\n",argument1);
+    printf("The argument2 is the command line argument: %s\n",argument2);
 
 
 
-    //enum is used to map meaningful names (words) into integer constants to make the code more readable
-    printf("\n");
-    printf(("\n"));
-    enum ROLES { ADMIN, USER, GUEST };
 
-    enum ROLES user_role = USER;
+    // enum companies {GOOGLE,FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT};
+    //
+    // //defining enums of type companies below
+    //
+    // enum companies google = GOOGLE;
+    // enum companies xerox  = XEROX;
+    // enum companies ebay   = EBAY;
+    //
+    // printf("The Company XEROX is: %d \n", xerox);
+    //
+    // printf("The Company Google is: %d \n", GOOGLE);
+    //
+    // printf("The Company EBAY is: %d \n", EBAY);
+    //
+    //
+    //
+    // //enum is used to map meaningful names (words) into integer constants to make the code more readable
+    // printf("\n");
+    // printf(("\n"));
+    // enum ROLES { ADMIN, USER, GUEST };
+    //
+    // enum ROLES user_role = USER;
+    //
+    // if (user_role == 0) {
+    //     printf("ADMIN SIGNED IN %d \n", user_role);
+    // } else if (user_role == 1) {
+    //     printf("NEW USER SIGNED IN %d \n", user_role);
+    // } else {
+    //     printf("GUEST USER SIGNED IN %d \n", user_role);
+    //
+    // }
 
-    if (user_role == 0) {
-        printf("ADMIN SIGNED IN %d \n", user_role);
-    } else if (user_role == 1) {
-        printf("NEW USER SIGNED IN %d \n", user_role);
-    } else {
-        printf("GUEST USER SIGNED IN %d \n", user_role);
 
-    }
 
 
 
